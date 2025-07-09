c765c7138b584912cba5044e64ca325fb5ed3131
zainqaimi <138384994+zainqaimi@users.noreply.github.com>
2025-07-10 00:27:49 +0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 export default function App() {
   return (
     <h1 className="text-3xl font-bold underline text-orange-500 text-center">
-      my-portfolio
+      my-portfolio working...
     </h1>
   );
 }
